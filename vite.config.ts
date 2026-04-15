a8ffce7032e469e5d3e7762c258fe33d63499909
Hinoya-Git <iancharles0608@gmail.com>
2026-04-15 16:33:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Aquino-Final_Project/'
+    base: '/Aquino-Final_Project/' ,
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
